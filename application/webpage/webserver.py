2cf2f88ac1776a11d46cacc467766aca9dfe396d
asketak <simatomas@seznam.cz>
2018-03-19 01:06:41 +0100

    zdroje btc ve frontendu

--- application/webpage/webserver.py
+++ application/webpage/webserver.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from flask import Flask, session, redirect, url_for, escape, request
 import json
 from pprint import pprint
@@ -5,6 +6,7 @@ from subprocess import check_output
 from flask import make_response
 from functools import wraps, update_wrapper
 from datetime import datetime
+import sys
 import requests
 
 def nocache(view):
@@ -38,73 +40,164 @@ def static_file(path):
     generateData(address);
     return app.send_static_file(path)
 
+def generateJson(rec,send):
+    tabrows = []
+
+    for row in rec:
+        print((row['row'][2]), file=sys.stderr)
+        distance = len(row['row'][2])/2/2 
+        url = row['row'][0]['link']
+        identity = row['row'][0]['name']
+        address = row['row'][1]['address']
+        outgoing = row['row'][2][0]['value']
+        tabrow = {}
+        tabrow['distance'] = distance
+        tabrow['url'] = url
+        tabrow['identity'] = identity
+        tabrow['address'] = address
+        tabrow['btc'] = outgoing
+        tabrows.append(tabrow)
+
+    for row in send:
+        # print((row['row'][2]), file=sys.stderr)
+        distance = -1*len(row['row'][2])/2/2 
+        url = row['row'][0]['link']
+        identity = row['row'][0]['name']
+        address = row['row'][1]['address']
+        outgoing = row['row'][2][0]['value']
+        tabrow = {}
+        tabrow['distance'] = distance
+        tabrow['url'] = url
+        tabrow['identity'] = identity
+        tabrow['address'] = address
+        tabrow['btc'] = outgoing
+        tabrows.append(tabrow)
+
+    return tabrows
+
+
+def parsetable(data):
+    send = data['results'][0]['data'] 
+    rec = data['results'][1]['data'] 
+    table_data_string = generateJson(rec,send) 
+    return table_data_string
+
+
+def parserecieved(data):
+    rec = data['results'][2]['data'][0]['row'][0]
+    print(rec, file=sys.stderr)
+    return rec
+
+def parsesent(data):
+    sent = data['results'][3]['data'][0]['row'][0]
+    return sent
+    
+
+def parsedist(data):
+    sent = data['results'][4]['data'] 
+    print(sent, file=sys.stderr)
+    # velikost adresy, pagerank, vzdalenost, groupa
+
+def compute_recieved(tabrows,recieved):
+    ret = "var piedata = "
+    rectot = 0
+    js = []
+    for row in tabrows:
+        if row['distance'] >0:
+            x = {}
+            x['label'] = row['identity']
+            x['value'] = row['btc']
+            rectot+=row['btc']
+            x['color'] = '#0600f3'
+            js.append(x)
+
+    x = {}
+    x['label'] = 'Unknown'
+    x['value'] = recieved -rectot
+    x['color'] = '#0600f3'
+    js.append(x)
+    ret += json.dumps(js)
+    return ret
+
+
 
 
 def generateData(address):
 
-    mock_data=""" var piedata = [ { "label": "Unknown", "value": 8, "color": "#f30000" }, { "label": "SatoshiDice", "value": 4, "color": "#0600f3" }, { "label": "Bitfinex", "value": 2, "color": "#00b109" }, { "label": "Addresses with identities", "value": 1, "color": "#67f200"
-    }]
-var distancedata=[ { x: 0, y: 0, c: 0, size: 0 }, { x: 2, y: 3, c: 0, size: 500 }, { x: 30, y: 70, c: 1, size: 800 }, { x: 5, y: 2, c: 2, size: 500 }, { x: 4, y: 4, c: 3, size: 1000 } ] var transactionsdata = { "nodes": [ {"id": "A1", "text": "vstupni TX", "group": 1, "value": 500, "value2": 500}, {"id": "A2", "text": "vstupni TX", "group": 1, "value": 500, "value2": 500}, {"id": "A3", "text": "vstupni TX", "group": 1, "value": 500, "value2": 500}, {"id": "A", "text": "Adresa", "group": 2, "value": 500, "value2": 500}, {"id": "B", "text": "vystupni TX", "group": 3, "value": 300, "value2": 300}, {"id": "C", "text": "vystupni TX", "group": 3, "value": 500, "value2": 500}, {"id": "D", "text": "vystupni TX", "group": 3, "value": 100, "value2": 100}, {"id": "E", "text": "vystupni TX", "group": 3, "value": 50, "value2": 50}, {"id": "F", "text": "vystupni TX", "group": 3, "value": 50, "value2": 50}, {"id": "G", "text": "vystupni TX", "group": 3, "value": 50, "value2": 50} 
+    mock_data=""" 
+
+var distancedata=[ { x: 0, y: 0, c: 0, size: 0 }, { x: 2, y: 3, c: 0, size: 500 }, { x: 30, y: 70, c: 1, size: 800 }, { x: 5, y: 2, c: 2, size: 500 }, { x: 4, y: 4, c: 3, size: 1000 } ] 
+
+var transactionsdata = { "nodes": [ {"id": "A1", "text": "vstupni TX", "group": 1, "value": 500, "value2": 500}, {"id": "A2", "text": "vstupni TX", "group": 1, "value": 500, "value2": 500}, {"id": "A3", "text": "vstupni TX", "group": 1, "value": 500, "value2": 500}, {"id": "A", "text": "Adresa", "group": 2, "value": 500, "value2": 500}, {"id": "B", "text": "vystupni TX", "group": 3, "value": 300, "value2": 300}, {"id": "C", "text": "vystupni TX", "group": 3, "value": 500, "value2": 500}, {"id": "D", "text": "vystupni TX", "group": 3, "value": 100, "value2": 100}, {"id": "E", "text": "vystupni TX", "group": 3, "value": 50, "value2": 50}, {"id": "F", "text": "vystupni TX", "group": 3, "value": 50, "value2": 50}, {"id": "G", "text": "vystupni TX", "group": 3, "value": 50, "value2": 50} 
   ], "links": [ {"source": "A1", "target": "A", "value": 1}, {"source": "A2", "target": "A", "value": 1}, {"source": "A3", "target": "A", "value": 1}, {"source": "A", "target": "B", "value": 1},
     {"source": "A", "target": "C", "value": 1}, {"source": "A", "target": "D", "value": 1}, {"source": "D", "target": "E", "value": 1}, {"source": "D", "target": "F", "value": 1},
-    {"source": "F", "target": "G", "value": 1} ]
+    {"source": "F", "target": "G", "value": 1} ] }
 
-}"""
-
-    refstring = """var tabledata=[
-    {
-        "address": "14BzdTwZyJTWewiZdYMGafiNUxmSYm9K91",
-        "distance": "7",
-        "identity": "Arnodl",
-        "url": "bitcointalk.com/profile=148",
-        "btc": "1.37",
-    },
-    {
-        "address": "343zdTXZyJTWewiZdYM33fiNUxmSYm3333",
-        "distance": "2",
-        "identity": "SlushPool",
-        "url": "slushpool.com",
-        "btc": "0.2",
-    }
-]
 """
 
-    tabledata_req =  """MATCH (start:Identity)-[]-(strt:Address )<-[:USES]-(o1:Output)
-             -[:INPUT|OUTPUT*1..6]->(o2:Output)-[:USES]->(end:Address {address: '1AEfmTfVsWMWhnw96FiF5jpzTy57wSQRjs'}),
-  p = shortestpath((o1)-[:INPUT|OUTPUT*1..6]->(o2))
-  with start,p
-  limit 1
-  return start,strt,p"""
+    tabledata_rec =  """MATCH (start:Identity)-[]-(strt:Address )<-[:USES]-(o1:Output)
+    -[:INPUT|OUTPUT*1..6]->(o2:Output)-[:USES]->(end:Address {address: '""" + address+ """'}),
+    p = shortestpath((o1)-[:INPUT|OUTPUT*1..6]->(o2))
+    with start,strt,p
+    limit 1
+    return start,strt,p"""
 
-    r = requests.post(' http://localhost:7474/db/data/transaction/commit', json={ "statements" : [ { "statement" : tabledata_req }, { "statement" : "match (n:Address) RETURN (n) limit 1" }] })
+    tabledata_send =  """MATCH (end:Address {address: '""" + address+ """'} )<-[:USES]-(o1:Output)
+    -[:INPUT|OUTPUT*1..6]->(o2:Output)-[:USES]->(strt:Address)-[]-(start:Identity),
+    p = shortestpath((o1)-[:INPUT|OUTPUT*1..6]->(o2))
+    with start,strt,p
+    limit 1
+    return start,strt,p"""
+
+    total_recieved =  """MATCH (a:Address {address: '""" + address + """' })<-[:USES]-(o),
+          (o)-[r:INPUT|OUTPUT]-(t)
+        WITH a, t,
+        CASE type(r) WHEN "OUTPUT" THEN sum(o.value) ELSE -sum(o.value) END AS value
+        WITH a, t,  sum(value) AS value
+            WHERE value > 0
+            RETURN sum(value);"""
+
+    total_sent =  """MATCH (a:Address {address: '""" + address + """' })<-[:USES]-(o)
+            WHERE (o)-[:INPUT]->()
+            RETURN sum(o.value)"""
+
+    distancedata_req =  """MATCH (end:Address {address: '""" + address+ """'} )<-[:USES]-(o1:Output)
+    -[:INPUT|OUTPUT*1..6]->(o2:Output)-[:USES]->(strt:Address)-[]-(start:Identity),
+    p = shortestpath((o1)-[:INPUT|OUTPUT*1..6]->(o2))
+    with start,strt,p
+    limit 1
+    return start,strt,p"""
+
+    r = requests.post(' http://localhost:7474/db/data/transaction/commit',
+     json={ "statements" : [ { "statement" : tabledata_send }
+    , { "statement" : tabledata_rec }
+    , { "statement" : total_recieved }
+    , { "statement" : total_sent }
+    , { "statement" : distancedata_req }
+     ]
+      })
+
     if(r.status_code != 200):
         return app.send_static_file("error.html")
 
-    data = r.json()
-    tabledata = r.json()['results'][0]['data'] #[0]['row'][0]
-    table_data_string = "var tabledata ="
-    i = 0
-    for row in tabledata:
-        distance = length(row['row'][2])
-        url = row['row'][0]['link']
-        identity = row['row'][0]['name']
-        address = row['row'][1]['address']
-        tabrows[i]['distance'] = distance
-        tabrows[i]['url'] = url
-        tabrows[i]['identity'] = identity
-        tabrows[i]['address'] = address
-        tabrows[i]['btc'] = 1
-        i += 1
-        print(address + "::::" + distance + "::::" + identity + "::::" + url + "::::" )
+    data = r.json() 
+    tabrows = parsetable(data)
+    table_data_string = "var tabledata =" 
+    table_data_string += json.dumps(tabrows)
 
-    table_data_string  += json.dumps(tabrows)
+    recieved = parserecieved(data)
+    # sent = parsesent(data)
+    # pie_sent_string = compute_sent(tabrows,sent)
+    pie_recieved_string = compute_recieved(tabrows,recieved)
+
+
+    distance_string = parsedist(data)
 
     text_file = open("static/data.js", "w")
-    text_file.write(mock_data + table_data_string)
+    text_file.write(mock_data + table_data_string + "\n" + pie_recieved_string)
     text_file.close()
 
 
 
 app.secret_key = 'L\xaf\xca\x1e(\x8dZ\xcc69\xe8\x19\x83\x80\xe9\x18\xe1L^]m\xab\x086'
 app.run(threaded=True)
-
