a6699808c1d34382db87e09b8ec1322f7ad1dc00
asketak <simatomas@seznam.cz>
2018-04-23 02:55:56 +0200

    text po fingerprinting

--- telnets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for (( i=1; i<=65535; i++))
-	do
- netcat -v -w 10 -xlocalhost:9050 -X5 t3e6ly3uoif4zcw2.onion $i
-done
--- telnets2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for (( i=79; i<=65535; i++))
-	do
-echo $i
- netcat -v -w 10 -xlocalhost:9050 -X5 valhallaxmn3fydu.onion $i
-done
