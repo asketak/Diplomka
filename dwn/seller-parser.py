7be81d031fe9673012e9202701828dc26830205b
asketak <simatomas@seznam.cz>
2018-04-27 12:47:10 +0200

    fix tahani feedbacku, snad uz opravdu finalni data :D

--- dwn/seller-parser.py
+++ dwn/seller-parser.py
@@ -10,14 +10,14 @@ orig_stdout = sys.stdout
 f = open('sellers.csv', 'w')
 sys.stdout = f
 
-for filename in os.listdir(spath):
-	vendor = filename
-	filename = spath + filename
-	revenue = subprocess.getoutput("""cat """ + filename + """ | grep "Total revenue" | cut -d':' -f2 | cut -d'<' -f1 """)
-	positive = subprocess.getoutput("""cat """ + filename + """ | grep -A 2 "Positive feedback:" | head -2 | tail -1 | cut -d'>' -f2 | cut -d'<' -f1 """)
-	negative = subprocess.getoutput("""cat """ + filename + """ | grep -A 2 "Negative feedback:" | head -2 | tail -1 | cut -d'>' -f2 | cut -d'<' -f1 """)
-	ships_from = subprocess.getoutput("""cat """ + filename + """ | grep "Ships from:" | head -1 | cut -d':' -f2 | cut -d'<' -f1 """)
-	print("'" + vendor + "'" + "," + revenue + "," + positive + "," + negative + "," + "'"+ ships_from + "'" )
+# for filename in os.listdir(spath):
+# 	vendor = filename
+# 	filename = spath + filename
+# 	revenue = subprocess.getoutput("""cat """ + filename + """ | grep "Total revenue" | cut -d':' -f2 | cut -d'<' -f1 """)
+# 	positive = subprocess.getoutput("""cat """ + filename + """ | grep -A 2 "Positive feedback:" | head -2 | tail -1 | cut -d'>' -f2 | cut -d'<' -f1 """)
+# 	negative = subprocess.getoutput("""cat """ + filename + """ | grep -A 2 "Negative feedback:" | head -2 | tail -1 | cut -d'>' -f2 | cut -d'<' -f1 """)
+# 	ships_from = subprocess.getoutput("""cat """ + filename + """ | grep "Ships from:" | head -1 | cut -d':' -f2 | cut -d'<' -f1 """)
+# 	print("'" + vendor + "'" + "," + revenue + "," + positive + "," + negative + "," + "'"+ ships_from + "'" )
 	
 sys.stdout.flush()
 f2 = open('feedbacks.csv', 'w')
@@ -25,7 +25,7 @@ sys.stdout = f2
 
 for filename in os.listdir(fpath):
 
-	vendor = filename
+	vendor = filename.split('.')[0]
 	filename = fpath + filename
 	buyer = subprocess.getoutput("""cat """ + filename + """ | grep "\*\*\*" | tr -d '*' """)
 	buyer = re.split('\n', buyer)
--- dwn/seller-scraper.py
+++ dwn/seller-scraper.py
@@ -2,25 +2,26 @@ from subprocess import call
 import subprocess
 
 bash1 = """ printf "http://valhallaxmn3fydu.onion/$filename \\n" | wget --load-cookies cookies.txt -P ./sellers/ -i- "$@" """
-bash2 = """ printf "http://valhallaxmn3fydu.onion/$filename/palautteet \\n" | wget --load-cookies cookies.txt -O ./feedbacks/$filename -i- "$@" """
+bash2 = """ printf "http://valhallaxmn3fydu.onion/$filename/palautteet \\n" | wget --load-cookies cookies.txt -O ./feedbacks/$filename -i- "$@" && cat ./feedbacks/$filename """
 
 
 x = []
 with open("listings.csv") as file:
 	for l in file:
-		seller_nick  = l.split("'")[1::2][0].replace('/', '')
+		seller_nick  = l.split("\"")[1::2][0].replace('/', '')
 		x.append(seller_nick)
 
 x = list(set(x))
+
 for seller_nick in x:
-	b1 = bash1.replace('$filename',seller_nick)
+	# b1 = bash1.replace('$filename',seller_nick)
 	b2 = bash2.replace('$filename',seller_nick)
-	process = subprocess.getoutput(b1)
+	# process = subprocess.getoutput(b1)
 	process = subprocess.getoutput(b2)
 
 	num=2
 	while """<span class="next">""" in process:
-		bash3 = """ printf "http://valhallaxmn3fydu.onion/$filename/palautteet/$num \\n" | wget --load-cookies cookies.txt -O ./feedbacks/$filename$num -i- "$@" """
+		bash3 = """ printf "http://valhallaxmn3fydu.onion/$filename/palautteet/$num \\n" | wget --load-cookies cookies.txt -O ./feedbacks/$filename.$num -i- "$@" && cat ./feedbacks/$filename.$num"""
 		b3 = bash3.replace('$filename',seller_nick).replace('$num',str(num))
 		num += 1
 		process = subprocess.getoutput(b3)
