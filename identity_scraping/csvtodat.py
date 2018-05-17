77289035c319f4ce4a4187de97dd806bad3cc2ef
asketak <simatomas@seznam.cz>
2018-05-17 23:11:55 +0200

    recieved fix

--- identity_scraping/csvtodat.py
+++ identity_scraping/csvtodat.py
@@ -2,30 +2,54 @@ import json
 import sys
 import os
 from pprint import pprint
-from tqdm import *
 import time
 from datetime import datetime, timedelta
 import subprocess
 
-os.path.isfile(addresses.csv)
-os.path.isfile(transactions.csv)
-os.path.isfile(outputs.csv)
-os.path.isfile(identities.csv)
-os.path.isfile(outputs-transactions.csv)
-os.path.isfile(outputs-addresses.csv)
+add = "addresses.csv"
+trans = "transactions.csv"
+outputs = "outputs.csv"
+ident = "identities.csv"
+outtran = "outputs-transactions.csv"
+outadd = "outputs-addresses.csv"
+blockdb = "./data/blockchain.db"
 
-importcomm = "neo4j-import --into ./data/blockchain.db "
+importcomm = "neo4j-import --into " + blockdb
+addimport = "--nodes:Address "+ add
+tranimport = "--nodes:Transaction "+ trans
+outimport = "--nodes:Outputs "+ outputs
+outtrans = "--relationships:Oinput "+ ident
+outaddd = "--relationships:Owns " + outtran
+identities = "--relationships:Identity " + ident
+okflag = True
 
---nodes:Address addresses.csv
---nodes:Transaction transactions.csv \
---nodes:Outputs outputs.csv \
---relationships:Oinput outputs-transactions.csv
---relationships:Owns outputs-addresses.csv
---relationships:Identity identities.csv
+def checkout(output, name):
+    if "IMPORT DONE" in output:
+        print(name+ " Import complete")
+    else:
+        print(name+ " Import ERROR")
+        print(output)
+        okflag = False
+ 
+    
 
-addresses = subprocess.getoutput(importcomm + addimport)
-transactions = subprocess.getoutput(importcomm + tranimport)
-outputs = subprocess.getoutput(importcomm + outimport)
-oinput = subprocess.getoutput(importcomm + outtrans)
-identity = subprocess.getoutput(importcomm + outaddd)
-identity = subprocess.getoutput(importcomm + identities)
+if os.path.isfile(add) and os.path.isfile(trans) and os.path.isfile(outputs) and os.path.isfile(ident) and os.path.isfile(outtran) and os.path.isfile(outadd):
+    out = subprocess.getoutput(importcomm + addimport)
+    checkout(out,"Address")
+    out = subprocess.getoutput(importcomm + tranimport)
+    checkout(out,"Transaction")
+    out = subprocess.getoutput(importcomm + outimport)
+    checkout(out,"Outputs")
+    out = subprocess.getoutput(importcomm + outtrans)
+    checkout(out,"Outputs-transactions relation")
+    out = subprocess.getoutput(importcomm + outaddd)
+    checkout(out,"Outputs-addresses relation")
+    out = subprocess.getoutput(importcomm + identities)
+    checkout(out,"Address-identities relation")
+    if okflag:
+        print("Import completed Succesfully")
+    else:
+        print("Import failed")
+
+
+
